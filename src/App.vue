f6f58e5645110f89e04270074cf22e2740cbe2b3
Dev-Bay.com <hello-contact@dev-bay.com>
2022-02-19 17:12:37 +0100

    FILES ADDED

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <SingleNewsBlock v-for="(news, i) in newsData" :key="i" :news="news" />
+    <footer>
+      <small>
+        * Photos from wikipedia: <a href="https://pl.wikipedia.org/wiki/Wroc%C5%82aw" target="_blank">Wroclaw</a>
+      </small>
+    </footer>
+  </div>
+</template>
+
+<script>
+import SingleNewsBlock from './components/SingleNewsBlock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SingleNewsBlock
+  },
+data () {
+  return {
+    newsData: [{
+      img: 'bridge.png',
+      title: "Bridge at night",
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper aliquet nulla a aliquam. Quisque sit amet fringilla justo. Cras ornare massa at dolor feugiat facilisis sed quis nunc. Suspendisse placerat euismod dictum.'
+    }, {
+      img: 'bridge2.png',
+      title: "Highway bridge",
+      desc: 'Duis placerat fermentum ipsum, nec vulputate mi blandit condimentum. In hac habitasse platea dictumst.'
+    }, {
+      img: 'building.png',
+      title: "Historic building",
+      desc: 'Vestibulum ac purus suscipit, aliquam nisi et, luctus arcu. Nam eget maximus est. Etiam sit amet accumsan arcu, ut imperdiet diam. Nulla facilisi'
+    }, {
+      img: 'skyscraper.png',
+      title: "Skyscraper",
+      desc: 'Aenean efficitur et ex vel luctus. Quisque commodo vel ex id consectetur. Sed vitae quam bibendum lectus tristique ultrices.'
+    }]
+  }
+}
+}
+</script>
+
+<style>
+#app {
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/SingleNewsBlock.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="news-block">
+    <div class="news-img" :style="backgroundImageInlineStyle">
+      <h2>{{news.title}}</h2>
+    </div>
+    <div class="news-description">
+      {{news.desc}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleNewsBlock',
+  props: {
+    news: Array
+  },
+  computed: {
+    backgroundImageInlineStyle () {
+      return `background-image: url("./assets/${this.news.img}");`;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  /**
+  * BACKGROUND IMAGE URL ELEMENT'S STYLES - BEGINING 
+  * POSITION BACKGROUND IMAGE TO ALWAYS COVER WHOLE BLOCK ELEMENT
+  * AND POSITION IT IN CENTER
+  * BACKGROUND IMAGE URL IS ADDED AS INLINE STYLE TO EACH ELEMENT IN HTML CODE ABOVE
+  */
+  .news-img {
+    height: 200px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+  /* BACKGROUND IMAGE URL ELEMENT'S STYLES - END */
+
+  .news-block {
+    background: orange;
+    width: 240px;
+    display: inline-block;
+    margin: 10px 5px;
+    padding: 5px;
+    box-sizing: border-box;
+    vertical-align: top;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+  .news-block h2 {
+    margin: 0;
+    text-align: center;
+    color: #fff;
+    text-shadow: 0 0 2px #000;;
+  }
+  .news-description {
+    padding: 5px;
+  }
+</style>
